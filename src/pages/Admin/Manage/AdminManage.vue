40050a7bd04dfa703b7d0978709cdf64b1f63d4d
LiuKairui <tclkr@live.com>
2022-02-17 22:09:58 +0800

    fix: manager

--- src/pages/Admin/Manage/AdminManage.vue
+++ src/pages/Admin/Manage/AdminManage.vue
@@ -11,14 +11,47 @@
 		</a-menu>
 		<div class="listWapper">
 			<div v-if="state.loading" class="loading"><a-spin size="large" /></div>
-			<div v-else class="content"></div>
+			<div v-else class="content">
+				<div class="toolBar">
+					<a-button danger ghost><close-outlined />删除分组</a-button>
+					<a-button type="primary" style="margin-right: 15px"><plus-outlined />新建分组</a-button>
+				</div>
+				<div class="userList">
+					<a-list class="demo-loadmore-list" :loading="loading" item-layout="horizontal" :data-source="dataList">
+						<!-- <template #loadMore>
+							<div :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
+								<a-spin v-if="loadingMore" />
+								<a-button v-else @click="loadMore">loading more</a-button>
+							</div>
+						</template> -->
+						<template #renderItem="{ item }">
+							<a-list-item>
+								<template #actions>
+									<a>edit</a>
+									<a>more</a>
+								</template>
+								<a-list-item-meta description="Ant Design, a design language for background applications, is refined by Ant UED Team">
+									<template #title>
+										<a href="https://www.antdv.com/">{{ item.name.last }}</a>
+									</template>
+									<template #avatar>
+										<a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+									</template>
+								</a-list-item-meta>
+								<div>content</div>
+							</a-list-item>
+						</template>
+					</a-list>
+				</div>
+				{{ state.aduitList }}
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { defineComponent, ref, onMounted, reactive } from 'vue';
-import { SlidersOutlined } from '@ant-design/icons-vue';
+import { PlusOutlined, CloseOutlined } from '@ant-design/icons-vue';
 import { useStore } from 'vuex';
 import { message } from 'ant-design-vue';
 import { adminApi } from '../../../api';
@@ -69,7 +102,25 @@ export default defineComponent({
 	.listWapper {
 		flex-grow: 1;
 		border-left: 1px solid #00000040;
-		background-color: #bfa;
+		padding: 20px;
+		.loading {
+			height: 100%;
+			width: 100%;
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+		}
+		.content {
+			width: 100%;
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			.toolBar {
+				width: 100%;
+				display: flex;
+				flex-direction: row-reverse;
+			}
+		}
 	}
 	.left-side-bar {
 		background-color: #f0f2f5ff;
